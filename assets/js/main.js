5b596bd94ef77140a22b8b137de54abd1c3e449f
Japneet Singh <japneetsingh03.js@gmail.com>
2023-04-21 22:31:29 -0400

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,96 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header =document.getElementById('header');
+    if(this.scrollY>=50) header.classList.add('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SWIPER POPULAR ===============*/
+var swiperPopular = new Swiper(".popular__container",{
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+/*=============== VALUE ACCORDION ===============*/
+const accordionitems = document.querySelectorAll('.value__accordion-item')
+
+accordionitems.forEach((item)=> {
+    const accordionHeader = item.querySelector('.value__accordion-header')
+
+    accordionHeader.addEventListener('click', () =>{
+        const openItem = document.querySelector('.accordion-open')
+        
+        toggleItem(item)
+
+        if(openItem && openItem!==item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) =>{
+    const accordionContent = item.querySelector('.value__accordion-content')
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }
+    else{
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+}
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 58
+    const sectionId = current.getAttribute('id')
+    const sectionLink = document.querySelector('.nav__menu a[href*="' + sectionId + '"]')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionLink.classList.add('active-link')
+    } else {
+      sectionLink.classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+}
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+sr.reveal(`.home__title, .popular__container, .subscribe__container, .footer__container`)
+sr.reveal(`.home__description, .footer__info`, {delay: 400})
+sr.reveal(`.home__search`, {delay: 500})
+sr.reveal(`.home__value`, {delay: 600})
+sr.reveal(`.home__images`, {delay: 700, origin:'bottom'})
+sr.reveal(`.logos_img`, {interval: 100})
+sr.reveal(`.value__images, .contact__content`, {origin: 'left'})
+sr.reveal(`.value__content, .contact__images`, {origin: 'right'})
